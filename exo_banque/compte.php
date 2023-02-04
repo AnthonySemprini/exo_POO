8a0185d896fda85ab959d39ee574b20033623f1b
Anthony <semprini.anthony@gmail.com>
2023-02-04 17:37:49 +0100

    compte ajout get et set + function credit et debit

--- exo_banque/compte.php
+++ exo_banque/compte.php
@@ -6,13 +6,16 @@ class Compte {
     private  float $_soldeInitial;
     private string $_devise;
     private  string $_titulaire;
+    private float $_status;
+        //construct
     
-    public function __construct($_libelle , $_soldeInitial , $_devise , $_titulaire)
+    public function __construct($_libelle , $_soldeInitial , $_devise , $_titulaire,$_status)
     {
         $this->_libelle = $_libelle;
         $this->_soldeInitial = $_soldeInitial;
         $this->_devise = $_devise;
         $this->_titulaire = $_titulaire;
+        $this->_status = 0;
     }
 	
     //getter
@@ -29,7 +32,9 @@ class Compte {
     public function getTitulaire(){
         return  $this->_titulaire;  
     }
-    
+    public function getStatus(){
+        return $this->_status;
+    }
         //setter
 
     public function setLibelle(){
@@ -44,4 +49,23 @@ class Compte {
     public function setTitulaire(){
         $this->_titulaire;
     }
+    public function setStatus(){
+        $this->_status;
+    }
+    public function crediter(float $crediter){
+        $this->_soldeInitial += $crediter;
+        echo "le compte de " . $this->_titulaire . $this->_libelle." :" . $this->_soldeInitial . "€<br>";
+    }
+    public function debiter(float $debiter){
+        $this->_soldeInitial -= $debiter;
+        echo "le compte de ". $this->_titulaire . $this->_libelle." :". $this->_soldeInitial . "€<br>";
+    }
+
 }
+
+$cp1 = new Compte ("livretA" , 100 , "€" , "Anthony" , 0);
+
+$cp1->crediter(200);
+$cp1->debiter(30);
+$cp1->crediter(4000);
+$cp1->debiter(5500);
